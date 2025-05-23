70b540ddbee5daafafac64487b0388d4324436f8
Owen Andrew Holt-Frank <oholtfrank3@login-phoenix-rh9-3.pace.gatech.edu>
2025-05-23 15:42:17 -0400

    indentation

--- src/tides/rt_cap_basis2.py
+++ src/tides/rt_cap_basis2.py
@@ -17,28 +17,28 @@ class MOCAP:
 			return np.stack([cap_alpha, cap_beta])
 
 
-def _calculate_cap_single(self, rt_scf, fock):
-	fock_trans = self.trans_fock(rt_scf, fock)
-	mo_energy, _ = np.linalg.eigh(fock_trans)
+	def _calculate_cap_single(self, rt_scf, fock):
+		fock_trans = self.trans_fock(rt_scf, fock)
+		mo_energy, _ = np.linalg.eigh(fock_trans)
 
-	damping_diagonal = []
-	for energy in mo_energy:
-		energy_corrected = energy - self.emin
-		if energy_corrected > 0:
-			damping_term = self.prefac * (1 - np.exp(self.expconst* energy_corrected))
-			if damping_term < (-1 * self.maxval):
-				damping_term = -1 * self.maxval
-			damping_diagonal.append(damping_term)
-		else:
-			damping_diagonal.append(0)
+		damping_diagonal = []
+		for energy in mo_energy:
+			energy_corrected = energy - self.emin
+			if energy_corrected > 0:
+				damping_term = self.prefac * (1 - np.exp(self.expconst* energy_corrected))
+				if damping_term < (-1 * self.maxval):
+					damping_term = -1 * self.maxval
+				damping_diagonal.append(damping_term)
+			else:
+				damping_diagonal.append(0)
 
-	damping_matrix = np.diag(np.array(damping_diagonal, dtype=np.complex128))
-	C_OAO = self.get_OAO_coeff(fock, rt_scf)
+		damping_matrix = np.diag(np.array(damping_diagonal, dtype=np.complex128))
+		C_OAO = self.get_OAO_coeff(fock, rt_scf)
 	#revert into the basis upon which we propogate in rt_scf
-	transform = np.linalg.inv(rt_scf.orth.T)
-	damping_OAO = np.dot(C_OAO, np.dot(damping_matrix, C_OAO.T.conj()))
-	damping_AO = np.dot(transform, np.dot(damping_OAO, transform.T))
-	return 1j * damping_AO
+		transform = np.linalg.inv(rt_scf.orth.T)
+		damping_OAO = np.dot(C_OAO, np.dot(damping_matrix, C_OAO.T.conj()))
+		damping_AO = np.dot(transform, np.dot(damping_OAO, transform.T))
+		return 1j * damping_AO
 
 	def get_OAO_coeff(self, fock, rt_scf):
 		raise NotImplementedError("Must choose choice of basis")
@@ -88,8 +88,8 @@ class FORTHO(MOCAP):
 	def __init__(self, expconst, emin, prefac=1, maxval=100):
 		super().__init__(expconst, emin, prefac, maxval)
 	def get_OAO_coeff(self, fock, rt_scf):
-		fock_orth - self.trans_fock(rt_scf, fock)
-		_, mo_coeff = np.linalg.eign(fock_orth)
+		fock_orth = self.trans_fock(rt_scf, fock)
+		_, mo_coeff = np.linalg.eigh(fock_orth)
 		return mo_coeff
 	def trans_fock(self, rt_scf, fock):
 		trans_fock = np.dot(rt_scf.orth.T, np.dot(fock, rt_scf.orth))
